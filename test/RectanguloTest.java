b322b89db40ea15d547f6df9564d7e99e9b1f330
luisjavierluna <javierlunaluis@gmail.com>
2023-07-17 17:16:20 -0600

    Video 2 - Testing con JUnit

--- test/RectanguloTest.java
+++ test/RectanguloTest.java
@@ -5,6 +5,7 @@
  */
 
 import junit.Entities.Rectangulo;
+import junit.services.RectanguloService;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -21,6 +22,8 @@ public class RectanguloTest {
     public RectanguloTest() {
     }
     
+    RectanguloService rs;
+    
     @BeforeClass
     public static void setUpClass() {
     }
@@ -31,6 +34,7 @@ public class RectanguloTest {
     
     @Before
     public void setUp() {
+        rs = new RectanguloService();
     }
     
     @After
@@ -40,5 +44,27 @@ public class RectanguloTest {
     @Test
     public void deberiaInicializarConColor() {
         assertNotNull(new Rectangulo(10, 10).getColor());
+    }
+    
+    @Test
+    public void deberíaCalcularArea() {
+        assertEquals(100, rs.calcularArea(new Rectangulo(10, 10)), 0);
+        assertEquals(20, rs.calcularArea(new Rectangulo(4, 5)), 0);
+        assertEquals(1, rs.calcularArea(new Rectangulo(1, 1)), 0);
+    }
+    
+    @Test
+    public void deberíaCalcularPerimetro() {
+        assertEquals(40, rs.calcularPerimetro(new Rectangulo(10, 10)), 0);
+        assertEquals(100, rs.calcularPerimetro(new Rectangulo(20, 30)), 0);
+        assertEquals(30, rs.calcularPerimetro(new Rectangulo(5, 10)), 0);
+    }
+    
+    @Test
+    public void deberíaActivarODesactivar() {
+        Rectangulo r = new Rectangulo(5, 5);
+        assertTrue(r.isActive());
+        r.setActive(false);
+        assertFalse(r.isActive());
     }
 }
